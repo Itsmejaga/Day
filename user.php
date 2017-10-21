a439462ea56639a6242da6efdb0f6c6ad57885bd
itsvimalraj <vimalraj@thinktower.in>
2017-10-21 10:48:34 +0530

    Vimal First Commit

---
+++ user.php
@@ -0,0 +1,12 @@
+    public function LeadExcelUpload() {
+        if (Session::get('loggedin_user_id')) {
+            $data['getprj'] = $this->_lead->getPrjList();
+
+            View::renderadmintemplate('HeaderNew', $data);
+            View::render('AccUserLevel/LeadExcelUpload', $data);
+            View::renderadmintemplate('Footer', $data);
+        } else {
+            Url::redirect('');
+            exit;
+        }
+    }
